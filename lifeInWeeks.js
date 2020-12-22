aa2a21d4fe54936d986401406141708b2370934a
@the_ladyking <40103113+Teresia-Kirungo@users.noreply.github.com>
2020-12-22 11:15:26 +0300

    Create lifeInWeeks.js

---
+++ lifeInWeeks.js
@@ -0,0 +1,22 @@
+function lifeInWeeks(age){
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    //supposing we are taking 120 as the full ripe age
+    // var age = prompt("How old are you?");
+    var ageRemaining = 120 - age;
+    var daysRemaining = ageRemaining * 365;
+    var weeksRemaining = ageRemaining * 52;    
+    var monthsRemaining = ageRemaining * 12;
+
+    console.log("You have " + daysRemaining + " days, " + weeksRemaining + " weeks, and " + monthsRemaining + " months left.");
+    
+    
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
